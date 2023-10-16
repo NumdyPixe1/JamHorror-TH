cfc349077114d108844ca91a840bc6e391557947
NumdyPixe1 <Numdao123@gmail.com>
2023-10-16 21:16:32 +0700

    Update day 4

--- JamHorror-TH/Assets/Scripts/BotRandom.cs
+++ JamHorror-TH/Assets/Scripts/BotRandom.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class BotRandom : MonoBehaviour
 {
+    public bool isWait = false;
+    Animator animator;
+    public GameObject item;
+    public GameObject rolling;
+
     public int randomBotDice;
+
     public List<GameObject> diceBot = new List<GameObject>();
     void Start()
     {
+        animator = GetComponent<Animator>();
+        rolling.SetActive(false);
+        item.SetActive(false);
+
         for (int i = 0; i < 3; i++)
         {
             diceBot[i].SetActive(false);
@@ -15,42 +25,56 @@ public class BotRandom : MonoBehaviour
     }
     public void BotRandomDice()
     {
-        randomBotDice = UnityEngine.Random.Range(0, 3);
+        // animator.SetTrigger("Rolling");
 
-        if (randomBotDice == 0)
+        if (isWait == false)
         {
+            rolling.SetActive(true);
+            item.SetActive(false);
+
+            randomBotDice = UnityEngine.Random.Range(0, 3);
             if (randomBotDice == 0)
             {
-                diceBot[2].SetActive(false);
-                diceBot[1].SetActive(true);
-                diceBot[0].SetActive(false);
-                Debug.Log("Bot got paper");
+                if (randomBotDice == 0)
+                {
+                    diceBot[2].SetActive(false);
+                    diceBot[1].SetActive(true);
+                    diceBot[0].SetActive(false);
+                    Debug.Log("Bot got paper");
+                }
             }
-        }
-        else if (randomBotDice == 1)
-        {
-            if (randomBotDice == 1)
+            else if (randomBotDice == 1)
             {
-                diceBot[2].SetActive(false);
-                diceBot[1].SetActive(false);
-                diceBot[0].SetActive(true);
-                Debug.Log("Bot got rock");
+                if (randomBotDice == 1)
+                {
+                    diceBot[2].SetActive(false);
+                    diceBot[1].SetActive(false);
+                    diceBot[0].SetActive(true);
+                    Debug.Log("Bot got rock");
+                }
+
             }
+            else if (randomBotDice == 2)
+            {
+                if (randomBotDice == 2)
+                {
+                    diceBot[2].SetActive(true);
+                    diceBot[1].SetActive(false);
+                    diceBot[0].SetActive(false);
+                    Debug.Log("Bot got scissors");
+                }
+            }
+            StartCoroutine(RollingAnimation());
 
         }
-        else if (randomBotDice == 2)
+        IEnumerator RollingAnimation()
         {
-            if (randomBotDice == 2)
-            {
-                diceBot[2].SetActive(true);
-                diceBot[1].SetActive(false);
-                diceBot[0].SetActive(false);
-                Debug.Log("Bot got scissors");
-            }
+            isWait = true;
+            yield return new WaitForSeconds(0.5f);
+            item.SetActive(true);
+            rolling.SetActive(false);
+            isWait = false;
         }
-
 
     }
-
-
 }
--- JamHorror-TH/Assets/Scripts/Random.cs
+++ JamHorror-TH/Assets/Scripts/Random.cs
@@ -1,84 +1,108 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 namespace A
 {
   public class Random : MonoBehaviour
   {
     public BotRandom botRandom;
-    [SerializeField] private int randomDice;
-    // [SerializeField] private Sprite[] sprite;
+    [SerializeField] static private int randomPlayerDice;
     public List<GameObject> dice = new List<GameObject>();
+    public GameObject item;
+    public GameObject rolling;
 
 
-    // [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private bool isCheckResult = false;
+    [SerializeField] private bool isWait = false;
+    Animator animator;
+
     void Start()
     {
+      animator = GetComponent<Animator>();
+      rolling.SetActive(false);
+      item.SetActive(false);
+
       for (int i = 0; i < 3; i++)
       {
         dice[i].SetActive(false);
       }
-      // spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     public void PickRandom()
     {
-      // for (int i = 0; i < 3; i++)
-
-      randomDice = UnityEngine.Random.Range(0, 3);
-
-      if (randomDice == 0)
+      if (isWait == false && isCheckResult == false)
       {
-        dice[2].SetActive(false);
-        dice[1].SetActive(true);
-        dice[0].SetActive(false);
-        Debug.Log("You got paper");
+        rolling.SetActive(true);
+        item.SetActive(false);
 
-      }
-      else if (randomDice == 1)
-      {
-        dice[2].SetActive(false);
-        dice[1].SetActive(false);
-        dice[0].SetActive(true);
-        Debug.Log("You got rock");
-      }
-      else if (randomDice == 2)
-      {
-        dice[2].SetActive(true);
-        dice[1].SetActive(false);
-        dice[0].SetActive(false);
-        Debug.Log("You got scissors");
+        randomPlayerDice = UnityEngine.Random.Range(0, 3);
+        if (randomPlayerDice == 0)//ออกกระดาษ
+        {
+          Debug.Log(randomPlayerDice);
+          dice[2].SetActive(false);
+          dice[1].SetActive(true);
+          dice[0].SetActive(false);
+          Debug.Log("You got paper");
+
+        }
+        else if (randomPlayerDice == 1)//ออกค้อน
+        {
+          Debug.Log(randomPlayerDice);
+          dice[2].SetActive(false);
+          dice[1].SetActive(false);
+          dice[0].SetActive(true);
+          Debug.Log("You got rock");
+
+        }
+        else if (randomPlayerDice == 2)//ออกกรรไกร
+        {
+          Debug.Log(randomPlayerDice);
+          dice[2].SetActive(true);
+          dice[1].SetActive(false);
+          dice[0].SetActive(false);
+          Debug.Log("You got scissors");
+
+        }
+        StartCoroutine(RollingAnimation());
       }
     }
-    private void Result()
+    IEnumerator RollingAnimation()
     {
-      if (randomDice == 1)
-      {
-        Debug.Log("You got paper");
-
-      }
-      // if (botRandom.randomBotDice == randomDice)
-      // {
-
-      // }
+      isWait = true;
+      isCheckResult = true;
+      Result();
+      yield return new WaitForSeconds(0.5f);
+      item.SetActive(true);
+      rolling.SetActive(false);
+      isWait = false;
+      isCheckResult = false;
     }
+    public void Result()
+    {
+
+      if (isCheckResult == true && isWait == true)
+      {
+        // Debug.Log(randomPlayerDice + " & " + botRandom.randomBotDice);
+        if (randomPlayerDice == 0 && botRandom.randomBotDice == 0) { Debug.Log("It's a tie!, You and bot selected paper" + randomPlayerDice + " & " + botRandom.randomBotDice); }
+        else if (randomPlayerDice == 0 && botRandom.randomBotDice == 1) { Debug.Log("You win!, Paper cover rock" + randomPlayerDice + " & " + botRandom.randomBotDice); }
+        else if (randomPlayerDice == 0 && botRandom.randomBotDice == 2) { Debug.Log("You lose!, Scissors cut paper" + randomPlayerDice + " & " + botRandom.randomBotDice); }
+
+        else if (randomPlayerDice == 1 && botRandom.randomBotDice == 0) { Debug.Log("You lose! Paper cover rock" + randomPlayerDice + " & " + botRandom.randomBotDice); }
+        else if (randomPlayerDice == 1 && botRandom.randomBotDice == 1) { Debug.Log("It's a tie!, You and bot selected rock" + randomPlayerDice + " & " + botRandom.randomBotDice); }
+        else if (randomPlayerDice == 1 && botRandom.randomBotDice == 2) { Debug.Log("You win!, Rock smashes scissors" + randomPlayerDice + " & " + botRandom.randomBotDice); }
+
+        else if (randomPlayerDice == 2 && botRandom.randomBotDice == 0) { Debug.Log("You lose!, Scissors cut paper" + randomPlayerDice + " & " + botRandom.randomBotDice); }
+        else if (randomPlayerDice == 2 && botRandom.randomBotDice == 1) { Debug.Log("You win!, Rock smashes scissors" + randomPlayerDice + " & " + botRandom.randomBotDice); }
+        else if (randomPlayerDice == 2 && botRandom.randomBotDice == 2) { Debug.Log("It's a tie!, You and bot selected scissors" + randomPlayerDice + " & " + botRandom.randomBotDice); }
+      }
+    }
+
 
     void Update()
     {
 
-
-
-
-
-
-
     }
-
-
-
-
-
-
-
   }
 }
+
